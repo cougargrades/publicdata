f91ee98e1b18117f085a4ca5798c045e1309ff05
Austin Jackson <themacphage@gmail.com>
2021-08-23 05:35:43 -0500

    change patchfile naming

--- bundler/bundle/patch/publications_core.py
+++ bundler/bundle/patch/publications_core.py
@@ -17,14 +17,14 @@ def generate(source: Path, destination: Path):
       core = [ row for row in reader ]
       # do corecourses first because these affect different documents
       for row in core:
-        with open(destination / f'patch-2-corecourses-{time_ns()}.json', 'w') as out:
+        with open(destination / f'patch_2_corecourses_{time_ns()}.json', 'w') as out:
           out.write(str(
             Patchfile(f'/catalog/{row["department"]} {row["catalogNumber"]}').append('groups', 'firebase.firestore.DocumentReference', f'/groups/{row["coreCode"]}')
           ))
       groups = list(set([ item["coreCode"] for item in core ]))
       for group in groups:
         coursesForGroup = [ item for item in core if item["coreCode"] == group]
-        with open(destination / f'patch-1-coregroups-{time_ns()}.json', 'w') as out:
+        with open(destination / f'patch_1_coregroups_{time_ns()}.json', 'w') as out:
           out.write(str(
             Patchfile(f'/groups/{group}').append('courses', 'firebase.firestore.DocumentReference', [ f'/catalog/{item["department"]} {item["catalogNumber"]}' for item in coursesForGroup ], many=True)
           ))
