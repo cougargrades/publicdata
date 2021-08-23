c889b2a10661f44dce4a5b85540dfcadc8f5558e
Austin Jackson <themacphage@gmail.com>
2021-08-23 02:50:04 -0500

    fixed #17

--- bundler/bundle/grade_distribution.py
+++ bundler/bundle/grade_distribution.py
@@ -1,3 +1,4 @@
+import os
 import csv
 from pathlib import Path
 from alive_progress import alive_bar
@@ -6,7 +7,7 @@ from alive_progress import alive_bar
 Combines all CSV data for this dataset into
 a single records.csv file
 '''
-def process(source: Path, destination: Path, csv_path_pattern: str = '*.csv') :
+def process(source: Path, destination: Path, csv_path_pattern: str = '*.csv'):
   csv_path_pattern = '*.csv' if csv_path_pattern == None else csv_path_pattern
   destination.mkdir(exist_ok=True)
   with alive_bar() as bar:
@@ -43,3 +44,43 @@ def process(source: Path, destination: Path, csv_path_pattern: str = '*.csv') :
                     result[field] = 0
                 writer.writerow(result)
                 bar()
+  print('Splitting records...')
+  split_csv(source_filepath=(destination / 'records.csv').absolute(), dest_folder=destination.absolute(), split_file_prefix='records_split', records_per_file=5000)
+  print('Done')
+
+def split_csv(source_filepath, dest_folder, split_file_prefix, records_per_file):
+    """
+    Split a source csv into multiple csvs of equal numbers of records,
+    except the last file.
+    Includes the initial header row in each split file.
+    Split files follow a zero-index sequential naming convention like so:
+      `{split_file_prefix}_0.csv`
+    From: https://stackoverflow.com/a/49452109
+    """
+    if records_per_file <= 0:
+      raise Exception('records_per_file must be > 0')
+
+    with open(source_filepath, 'r') as source:
+      reader = csv.reader(source)
+      headers = next(reader)
+      file_idx = 0
+      records_exist = True
+      while records_exist:
+        i = 0
+        target_filename = f'{split_file_prefix}_{file_idx}.csv'
+        target_filepath = os.path.join(dest_folder, target_filename)
+        with open(target_filepath, 'w') as target:
+            writer = csv.writer(target)
+            while i < records_per_file:
+              if i == 0:
+                writer.writerow(headers)
+              try:
+                writer.writerow(next(reader))
+                i += 1
+              except StopIteration:
+                records_exist = False
+                break
+        if i == 0:
+          # we only wrote the header, so delete that file
+          os.remove(target_filepath)
+        file_idx += 1
