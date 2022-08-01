6d100caf07b99bef45c97cd954241e6aaa8bb0bc
Austin Jackson <themacphage@gmail.com>
2022-08-01 14:59:14 -0500

    added new core curriculum metadata

--- documents/edu.uh.publications.core/src/scrape.py
+++ documents/edu.uh.publications.core/src/scrape.py
@@ -78,38 +78,45 @@ with open('../core_curriculum.csv', 'w') as export:
     
     # iterate over catoids
     for catoid in records.keys():
-      for coreCode in records[catoid].keys():
-        id = records[catoid][coreCode]
-        res = requests.get(f'http://publications.uh.edu/ajax/preview_page.php?catoid={catoid}&id={id}&show')
-        if res.status_code == 200:
-          html = BeautifulSoup(res.content.decode(), features='html5lib')
-          # document.querySelector('h2')
-          coreArea = html.select_one('h2').text.split(':')[0].strip()
-          # document.querySelectorAll('h2 ~ ul li a:not(h4 ~ ul li a)')
-          links = html.select('h2 ~ ul li a:not(h4 ~ ul li a)')
-          for anchor in links:
-            result = dict.fromkeys(master.fieldnames)
-            # catoid,coreCode,coreArea
-            result['catoid'] = catoid
-            result['coreCode'] = coreCode
-            result['coreArea'] = coreArea
-            # department,catalogNumber,description,
-            anchorText = anchor.text.strip()
-            courseName = anchorText[:anchorText.index('-')].strip()
-            description = anchorText[anchorText.index('-')+1:].strip()
-            result['description'] = description.strip()
-            department, catalogNumber = courseName.strip().split(' ')
-            result['department'] = department.strip()
-            result['catalogNumber'] = catalogNumber.strip()
-            # coid
-            coid = parse_qs(urlparse(anchor['href']).query)['coid'][0]
-            result['coid'] = coid
+      res2 = requests.get(f'http://publications.uh.edu/content.php?catoid={catoid}&navoid={id}')
+      if res2.status_code == 200:
+        html2 = BeautifulSoup(res2.content.decode(), features='html5lib')
+        catalogName = html2.select_one('span.acalog_catalog_name').text
+        print(f'--- \'{catalogName}\' ---')
+        for coreCode in records[catoid].keys():
+          id = records[catoid][coreCode]
+          res = requests.get(f'http://publications.uh.edu/ajax/preview_page.php?catoid={catoid}&id={id}&show')
+          if res.status_code == 200:
+            html = BeautifulSoup(res.content.decode(), features='html5lib')
+            # document.querySelector('h2')
+            coreArea = html.select_one('h2').text.split(':')[0].strip()
+            # document.querySelectorAll('h2 ~ ul li a:not(h4 ~ ul li a)')
+            links = html.select('h2 ~ ul li a:not(h4 ~ ul li a)')
+            for anchor in links:
+              result = dict.fromkeys(master.fieldnames)
+              # catoid,coreCode,coreArea
+              result['catoid'] = catoid
+              result['coreCode'] = coreCode
+              result['coreArea'] = coreArea
+              # department,catalogNumber,description,
+              anchorText = anchor.text.strip()
+              courseName = anchorText[:anchorText.index('-')].strip()
+              description = anchorText[anchorText.index('-')+1:].strip()
+              result['description'] = description.strip()
+              department, catalogNumber = courseName.strip().split(' ')
+              result['department'] = department.strip()
+              result['catalogNumber'] = catalogNumber.strip()
+              # coid
+              coid = parse_qs(urlparse(anchor['href']).query)['coid'][0]
+              result['coid'] = coid
+              result['groupNavoid'] = id
+              result['groupTitle'] = catalogName
 
-            # used for de-duping (https://stackoverflow.com/a/12851143/4852536)
-            # we don't care if a course with the same name appears in multiple catalogs
-            key = (result['department'], result['catalogNumber'], coreCode)
-            if key not in entries:
-              writer.writerow(result)
-              entries.add(key)
-          print(f'[{catoid}] {coreArea} done')
-          sleep(1)
+              # used for de-duping (https://stackoverflow.com/a/12851143/4852536)
+              # we don't care if a course with the same name appears in multiple catalogs
+              key = (result['department'], result['catalogNumber'], coreCode)
+              if key not in entries:
+                writer.writerow(result)
+                entries.add(key)
+            print(f'[{catoid}] {coreArea} done')
+            sleep(1)
