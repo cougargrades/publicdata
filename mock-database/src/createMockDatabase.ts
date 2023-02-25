e396bd22b27385abec4e04f55f3e9c8b5ec2348b
Austin Jackson <themacphage@gmail.com>
2023-02-24 21:54:03 -0600

    reduced logging because it lags github actions

--- mock-database/src/createMockDatabase.ts
+++ mock-database/src/createMockDatabase.ts
@@ -21,16 +21,23 @@ console.log('CSV records have been parsed and shuffled');
 
 await reset()
 
+
+const prepatch = files.filter(e => e.split('/').reverse()[0].startsWith(`patch-0`))
+console.log(`Pre-processing patch-0 patchfiles (${prepatch.length} entries)...`)
 // Do `patch-0` first
-for (let file of files.filter(e => e.split('/').reverse()[0].startsWith(`patch-0`))) {
+for (let file of prepatch) {
   await processPatchfile(file)
 }
+console.log(`Pre-processed patch-0 patchfiles (${prepatch.length} processed)`)
 
+console.log(`Starting record processing (${records.length} entries)...`)
+console.time('Records processed in')
 // Do `records.csv`
 for (let i = 0; i < records.length; i++) {
   await whenUploadQueueAdded(records[i]);
-  console.log(`Processed record: ${i+1} of ${records.length} (${((i+1)/records.length*100).toFixed(1)}%)`)
+  //console.log(`Processed record: ${i+1} of ${records.length} (${((i+1)/records.length*100).toFixed(1)}%)`)
 }
+console.timeEnd('Records processed in')
 console.log(`Finished processing records (${records.length} processed)`)
 
 // Do remaining patchfiles
