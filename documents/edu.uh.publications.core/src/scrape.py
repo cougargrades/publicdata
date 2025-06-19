83b5c9fc3dada9ccf3711b978fd3ad34580379a0
Austin Jackson <austinjckson@gmail.com>
2025-06-19 14:00:53 -0500

    re-scraped: edu.uh.publications.core

--- documents/edu.uh.publications.core/src/scrape.py
+++ documents/edu.uh.publications.core/src/scrape.py
@@ -4,6 +4,7 @@ import requests
 from urllib.parse import urlparse, parse_qs
 from time import sleep
 from bs4 import BeautifulSoup
+import analyze
 
 # http://publications.uh.edu/ajax/preview_page.php?catoid=36&id=13126&show
 # h2
@@ -22,101 +23,111 @@ from bs4 import BeautifulSoup
 91 => Math/Reasoning
 '''
 
-# ids[catoid][coreCode][id]
+
+# ids[catoid]["core"][coreCode][api_core_id]
 records = {
-  # 2020-2022 Undergraduate
-  36: {
-    10: 13121,
-    20: 13122,
-    30: 13123,
-    40: 13120,
-    50: 13124,
-    60: 13125,
-    70: 13126,
-    80: 13127,
-    81: 13129,
-    91: 13128,
+  # 2024-2025 Undergraduate
+  52: {
+    # https://publications.uh.edu/content.php?catoid=52&navoid=20440
+    "root_navoid": 20440,
+    "core": {
+      10: 69061, # Communication 010 (6 credit hours) 
+      20: 69062, # Mathematics 020 (3 credit hours)
+      30: 69063, # Life & Physical Sciences 030 (6 credit hours)
+      40: 69064, # Language, Philosophy, and Culture 040 (3 credit hours)
+      50: 69065, # Creative Arts 050 (3 credit hours) 
+      60: 69066, # American History 060 (6 credit hours)
+      70: 69067, # Government/Political Science 070 (6 credit hours)
+      80: 69068, # Social and Behavioral Sciences 080 (3 credit hours)
+      81: 69070, # Component Area Option 090 (B): Writing in the Disciplines UH 081 (3 credit hours)
+      91: 69069, # Component Area Option 090 (A): Mathematics/Reasoning (3 credit hours)
+    }
   },
-  # 2021-2022 Undergraduate
-  41: {
-    10: 14726, # http://publications.uh.edu/content.php?catoid=41&navoid=14726
-    20: 14727, # http://publications.uh.edu/content.php?catoid=41&navoid=14727
-    30: 14728, # http://publications.uh.edu/content.php?catoid=41&navoid=14728
-    40: 14725, # http://publications.uh.edu/content.php?catoid=41&navoid=14725
-    50: 14729, # http://publications.uh.edu/content.php?catoid=41&navoid=14729
-    60: 14730, # http://publications.uh.edu/content.php?catoid=41&navoid=14730
-    70: 14731, # http://publications.uh.edu/content.php?catoid=41&navoid=14731
-    80: 14732, # http://publications.uh.edu/content.php?catoid=41&navoid=14732
-    81: 14734, # http://publications.uh.edu/content.php?catoid=41&navoid=14734
-    91: 14733, # http://publications.uh.edu/content.php?catoid=41&navoid=14733
-  },
-  # 2022-2023 Undergraduate
-  44: {
-    10: 15888, # http://publications.uh.edu/content.php?catoid=44&navoid=15888
-    20: 15889, # http://publications.uh.edu/content.php?catoid=44&navoid=15889
-    30: 15890, # http://publications.uh.edu/content.php?catoid=44&navoid=15890
-    40: 15887, # http://publications.uh.edu/content.php?catoid=44&navoid=15887
-    50: 15891, # http://publications.uh.edu/content.php?catoid=44&navoid=15891
-    60: 15892, # http://publications.uh.edu/content.php?catoid=44&navoid=15892
-    70: 15893, # http://publications.uh.edu/content.php?catoid=44&navoid=15893
-    80: 15894, # http://publications.uh.edu/content.php?catoid=44&navoid=15894
-    81: 15896, # http://publications.uh.edu/content.php?catoid=44&navoid=15896
-    91: 15895, # http://publications.uh.edu/content.php?catoid=44&navoid=15895
-  }
+
 }
 
-with open('../core_curriculum.csv', 'w') as export:
-  with open('../master.csv', 'r') as masterFile:
-    # declare writer
-    master = csv.DictReader(masterFile)
-    writer = csv.DictWriter(export, master.fieldnames)
-    # write the header row
-    writer.writeheader()
+'''
 
-    # used for de-duping
-    entries = set()
-    
-    # iterate over catoids
-    for catoid in records.keys():
-      res2 = requests.get(f'http://publications.uh.edu/content.php?catoid={catoid}&navoid={id}')
-      if res2.status_code == 200:
-        html2 = BeautifulSoup(res2.content.decode(), features='html5lib')
-        catalogName = html2.select_one('span.acalog_catalog_name').text
+{
+  "department": "ENGL", -> comes from course title
+  "catalogNumber": "2361", -> comes from course title
+  "description": "Western World Literature II - Honors", -> comes from course title
+  "catoid": "36", -> comes from catalog
+  "coid": "171276", -> comes from course 
+  "coreCode": "10", -> decided by the programmer (see above)
+  "coreArea": "Communication", -> comes from core (this is not used in the website)
+  "groupNavoid": "13121", -> comes from original page (root_navoid)
+  "groupTitle": "2020-2021 Undergraduate Catalog" -> comes from catalog
+},
+
+'''
+
+with open('../core_curriculum_old.csv', 'r') as legacyFile:
+  # newline is needed, see: https://stackoverflow.com/a/3348664/4852536
+  with open('../core_curriculum.csv', 'w', newline='') as export:
+    with open('../master.csv', 'r') as masterFile:
+      # declare writer
+      master = csv.DictReader(masterFile)
+      writer = csv.DictWriter(export, master.fieldnames)
+
+      # write the header row
+      writer.writeheader()
+
+      # used for de-duping
+      entries = set()
+
+      print(f'--- (Legacy scrape data) ---')
+      # write legacy data into the output
+      for item in csv.DictReader(legacyFile):
+        writer.writerow(item)
+      
+      # iterate over catoids
+      for catoid in records.keys():
+
+        # get catalog information
+        shallow_catalog = analyze.get_catalog(legacy_catalog_id=catoid)
+        catalog_id = shallow_catalog["id"]
+        catalogName = shallow_catalog["name"]
+        groupNavoid = records[catoid]["root_navoid"]
+
         print(f'--- \'{catalogName}\' ---')
-        for coreCode in records[catoid].keys():
-          id = records[catoid][coreCode]
-          res = requests.get(f'http://publications.uh.edu/ajax/preview_page.php?catoid={catoid}&id={id}&show')
-          if res.status_code == 200:
-            html = BeautifulSoup(res.content.decode(), features='html5lib')
-            # document.querySelector('h2')
-            coreArea = html.select_one('h2').text.split(':')[0].strip()
-            # document.querySelectorAll('h2 ~ ul li a:not(h4 ~ ul li a)')
-            links = html.select('h2 ~ ul li a:not(h4 ~ ul li a)')
-            for anchor in links:
-              result = dict.fromkeys(master.fieldnames)
-              # catoid,coreCode,coreArea
-              result['catoid'] = catoid
-              result['coreCode'] = coreCode
-              result['coreArea'] = coreArea
-              # department,catalogNumber,description,
-              anchorText = anchor.text.strip()
-              courseName = anchorText[:anchorText.index('-')].strip()
-              description = anchorText[anchorText.index('-')+1:].strip()
-              result['description'] = description.strip()
-              department, catalogNumber = courseName.strip().split(' ')
-              result['department'] = department.strip()
-              result['catalogNumber'] = catalogNumber.strip()
-              # coid
-              coid = parse_qs(urlparse(anchor['href']).query)['coid'][0]
-              result['coid'] = coid
-              result['groupNavoid'] = id
-              result['groupTitle'] = catalogName
 
-              # used for de-duping (https://stackoverflow.com/a/12851143/4852536)
-              # we don't care if a course with the same name appears in multiple catalogs
-              key = (result['department'], result['catalogNumber'], coreCode, id)
-              if key not in entries:
-                writer.writerow(result)
-                entries.add(key)
-            print(f'[{catoid}] {coreArea} done')
-            sleep(1)
+        # iterate over coreCodes
+        for coreCode in records[catoid]["core"].keys():
+          core_id = records[catoid]["core"][coreCode]
+          # fetch data for a `core`
+          deep_core = analyze.get_core(catalog_id=catalog_id, core_id=core_id)
+          core_courses = analyze.get_core_courses(catalog_id=catalog_id, core_id=core_id)
+          coreArea = deep_core["name"]
+
+          # iterate over courses
+          for course in core_courses:
+            result = dict.fromkeys(master.fieldnames)
+            # catoid,coreCode,coreArea
+            result['catoid'] = catoid
+            result['coreCode'] = coreCode
+            result['coreArea'] = coreArea.strip()
+
+            #course["title"] ==> "HIST 1301 -  The U.S. to 1877"
+
+            # department,catalogNumber,description,
+            anchorText = course["title"].strip()
+            courseName = anchorText[:anchorText.index('-')].strip()
+            description = anchorText[anchorText.index('-')+1:].strip()
+            result['description'] = description.strip()
+            department, catalogNumber = courseName.strip().split(' ')
+            result['department'] = department.strip()
+            result['catalogNumber'] = catalogNumber.strip()
+
+            result['coid'] = course["legacy-id"]
+            result['groupNavoid'] = groupNavoid
+            result['groupTitle'] = catalogName.strip()
+
+            # used for de-duping (https://stackoverflow.com/a/12851143/4852536)
+            # we don't care if a course with the same name appears in multiple catalogs
+            key = (result['department'], result['catalogNumber'], coreCode, groupNavoid)
+            if key not in entries:
+              writer.writerow(result)
+              entries.add(key)
+          
+          print(f'[{catoid}] {coreArea} done')
---
+++ documents/edu.uh.publications.core/src/scrape_old.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+import csv
+import requests
+from urllib.parse import urlparse, parse_qs
+from time import sleep
+from bs4 import BeautifulSoup
+
+# http://publications.uh.edu/ajax/preview_page.php?catoid=36&id=13126&show
+# h2
+# document.querySelectorAll('h2 ~ ul li a:not(h4 ~ ul li a)')
+
+'''
+10 => Communication
+20 => Mathematics
+30 => Life & Physical Sciences
+40 => Language, Philosophy, & Culture
+50 => Creative Arts
+60 => American History
+70 => Government/Political Science
+80 => Social & Behavioral Sciences
+81 => Writing in the Disciplines
+91 => Math/Reasoning
+'''
+
+# ids[catoid][coreCode][navoid]
+records = {
+  # 2020-2022 Undergraduate
+  36: {
+    10: 13121,
+    20: 13122,
+    30: 13123,
+    40: 13120,
+    50: 13124,
+    60: 13125,
+    70: 13126,
+    80: 13127,
+    81: 13129,
+    91: 13128,
+  },
+  # 2021-2022 Undergraduate
+  41: {
+    10: 14726, # http://publications.uh.edu/content.php?catoid=41&navoid=14726
+    20: 14727, # http://publications.uh.edu/content.php?catoid=41&navoid=14727
+    30: 14728, # http://publications.uh.edu/content.php?catoid=41&navoid=14728
+    40: 14725, # http://publications.uh.edu/content.php?catoid=41&navoid=14725
+    50: 14729, # http://publications.uh.edu/content.php?catoid=41&navoid=14729
+    60: 14730, # http://publications.uh.edu/content.php?catoid=41&navoid=14730
+    70: 14731, # http://publications.uh.edu/content.php?catoid=41&navoid=14731
+    80: 14732, # http://publications.uh.edu/content.php?catoid=41&navoid=14732
+    81: 14734, # http://publications.uh.edu/content.php?catoid=41&navoid=14734
+    91: 14733, # http://publications.uh.edu/content.php?catoid=41&navoid=14733
+  },
+  # 2022-2023 Undergraduate
+  44: {
+    10: 15888, # http://publications.uh.edu/content.php?catoid=44&navoid=15888
+    20: 15889, # http://publications.uh.edu/content.php?catoid=44&navoid=15889
+    30: 15890, # http://publications.uh.edu/content.php?catoid=44&navoid=15890
+    40: 15887, # http://publications.uh.edu/content.php?catoid=44&navoid=15887
+    50: 15891, # http://publications.uh.edu/content.php?catoid=44&navoid=15891
+    60: 15892, # http://publications.uh.edu/content.php?catoid=44&navoid=15892
+    70: 15893, # http://publications.uh.edu/content.php?catoid=44&navoid=15893
+    80: 15894, # http://publications.uh.edu/content.php?catoid=44&navoid=15894
+    81: 15896, # http://publications.uh.edu/content.php?catoid=44&navoid=15896
+    91: 15895, # http://publications.uh.edu/content.php?catoid=44&navoid=15895
+  }
+}
+
+# newline is needed, see: https://stackoverflow.com/a/3348664/4852536
+with open('../core_curriculum_old.csv', 'w', newline='') as export:
+  with open('../master.csv', 'r') as masterFile:
+    # declare writer
+    master = csv.DictReader(masterFile)
+    writer = csv.DictWriter(export, master.fieldnames)
+    # write the header row
+    writer.writeheader()
+
+    # used for de-duping
+    entries = set()
+    
+    # iterate over catoids
+    for catoid in records.keys():
+      res2 = requests.get(f'https://publications.uh.edu/content.php?catoid={catoid}&navoid={id}', verify=False)
+      if res2.status_code == 200:
+        html2 = BeautifulSoup(res2.content.decode(), features='html5lib')
+        catalogName = html2.select_one('span.acalog_catalog_name').text
+        print(f'--- \'{catalogName}\' ---')
+        for coreCode in records[catoid].keys():
+          id = records[catoid][coreCode]
+          res = requests.get(f'https://publications.uh.edu/ajax/preview_page.php?catoid={catoid}&id={id}&show', verify=False)
+          if res.status_code == 200:
+            html = BeautifulSoup(res.content.decode(), features='html5lib')
+            # document.querySelector('h2')
+            coreArea = html.select_one('h2').text.split(':')[0].strip()
+            # document.querySelectorAll('h2 ~ ul li a:not(h4 ~ ul li a)')
+            links = html.select('h2 ~ ul li a:not(h4 ~ ul li a)')
+            for anchor in links:
+              result = dict.fromkeys(master.fieldnames)
+              # catoid,coreCode,coreArea
+              result['catoid'] = catoid
+              result['coreCode'] = coreCode
+              result['coreArea'] = coreArea
+              # department,catalogNumber,description,
+              anchorText = anchor.text.strip()
+              courseName = anchorText[:anchorText.index('-')].strip()
+              description = anchorText[anchorText.index('-')+1:].strip()
+              result['description'] = description.strip()
+              department, catalogNumber = courseName.strip().split(' ')
+              result['department'] = department.strip()
+              result['catalogNumber'] = catalogNumber.strip()
+              # coid
+              coid = parse_qs(urlparse(anchor['href']).query)['coid'][0]
+              result['coid'] = coid
+              result['groupNavoid'] = id
+              result['groupTitle'] = catalogName
+
+              # used for de-duping (https://stackoverflow.com/a/12851143/4852536)
+              # we don't care if a course with the same name appears in multiple catalogs
+              key = (result['department'], result['catalogNumber'], coreCode, id)
+              if key not in entries:
+                writer.writerow(result)
+                entries.add(key)
+            print(f'[{catoid}] {coreArea} done')
+            sleep(1)
