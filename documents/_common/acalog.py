7a08f08e71a1dd01e13bae6b7ed1d99725f63d44
Austin Jackson <themacphage@gmail.com>
2025-07-21 12:23:26 -0700

    research #49

--- documents/_common/acalog.py
+++ documents/_common/acalog.py
@@ -8,15 +8,25 @@ from typing import Generator, Generic, TypeVar, Union
 import http.client
 import json
 import math
+import os
+import time
 
 '''
 HTTP REQUEST HELPERS
 '''
 
-def http_request(req: Request) -> http.client.HTTPResponse:
+IS_DEBUGGING = "DEBUG" in os.environ and f'{os.environ["DEBUG"]}'.lower() == "true"
+
+def http_request(req: Union[Request, str]) -> http.client.HTTPResponse:
+  method = req.method if type(req) == Request else 'GET'
+  url = req.get_full_url() if type(req) == Request else req
+  if IS_DEBUGGING:
+    print(f'HTTP {method} -> {url}')
   res = urlopen(req)
   # if type(res) == http.client.HTTPResponse:
   #   res.ok = res.status < 400
+  if IS_DEBUGGING:
+    print(f'HTTP {res.status} <- {url}')
   return res
 
 def is_ok(res: http.client.HTTPResponse) -> bool:
@@ -29,7 +39,8 @@ def http_request_json(req: Union[Request, str]) -> any:
   if type(req) == str:
     req = Request(method='GET', url=req)
   res = http_request(req)
-  with urlopen(req) as res:
+  #urlopen(req) as
+  with res:
     if type(res) == http.client.HTTPResponse:
       return json.load(res)
     else:
@@ -38,6 +49,42 @@ def http_request_json(req: Union[Request, str]) -> any:
 '''
 DOMAIN-SPECIFIC FUNCTIONS
 '''
+
+def get_catalogs(legacy_id: int = None, type: str = None, name: str = None) -> Generator[any, None, None]:
+  '''
+  Get all courses in a catalog (results will be yielded as they are found)
+  :param legacy_id: Acalog legacy ID visible in the website
+  :param type: Ex: "Undergraduate", exact math
+  :param name: Ex: "2024-2025 Undergraduate Catalog", exact match
+  '''
+  i = 0
+  query = {
+    "page": 1,
+    "legacy-id": legacy_id,
+    "type": type,
+    "name": name,
+  }
+  # remove keys with "None"
+  for k in list(query.keys()):
+    if query[k] == None:
+      query.pop(k)
+  data = http_request_json(f'https://uh.catalog.acalog.com/widget-api/catalogs/?page-size=100&{urlencode(query)}')
+  n = data["count"]
+
+  # This endpoint has completely broken pagination, so we're not doing that
+  for item in data["catalog-list"]:
+    yield (item, i, n)
+    i += 1
+  
+  return
+
+  # Loop until the API returns no more data
+  # while len(data["catalog-list"]) > 0:
+  #   for item in data["catalog-list"]:
+  #     yield (item, i, n)
+  #     i += 1
+  #   query["page"] += 1
+  #   data = http_request_json(f'https://uh.catalog.acalog.com/widget-api/catalogs/?page-size=100&{urlencode(query)}')
 
 def get_catalog(legacy_catalog_id: int) -> any:
   '''
@@ -115,22 +162,39 @@ def get_core_courses(catalog_id: int, core_id: int) -> list[any]:
   else:
     return None
 
-def get_courses(catalog_id: int) -> Generator[any, None, None]:
+def get_courses(catalog_id: int, legacy_id: int = None, type: str = None, prefix: str = None, code: str = None, name: str = None) -> Generator[any, None, None]:
   '''
   Get all courses in a catalog (results will be yielded as they are found)
+  :param legacy_id: Acalog legacy ID visible in the website
+  :param type: Ex: "Asian American Studies", exact math
+  :param prefix: Ex: "MATH", exact match
+  :param code: Ex: "1100", exact match
+  :param name: Ex: "Introduction to Asian American Studies", exact match
   '''
+
   i = 0
-  page = 1
-  data = http_request_json(f'https://uh.catalog.acalog.com/widget-api/catalog/{catalog_id}/courses/?page-size=20&page={page}')
+  query = {
+    "page": 1,
+    "legacy-id": legacy_id,
+    "type": type,
+    "prefix": prefix,
+    "code": code,
+    "name": name,
+  }
+  # remove keys with "None"
+  for k in list(query.keys()):
+    if query[k] == None:
+      query.pop(k)
+  data = http_request_json(f'https://uh.catalog.acalog.com/widget-api/catalog/{catalog_id}/courses/?page-size=100&{urlencode(query)}')
   n = data["count"]
   
   # Loop until the API returns no more data
   while len(data["course-list"]) > 0:
     for item in data["course-list"]:
       yield (item, i, n)
-      i += 1 
-    page += 1
-    data = http_request_json(f'https://uh.catalog.acalog.com/widget-api/catalog/{catalog_id}/courses/?page-size=20&page={page}')
+      i += 1
+    query["page"] += 1
+    data = http_request_json(f'https://uh.catalog.acalog.com/widget-api/catalog/{catalog_id}/courses/?page-size=100&{urlencode(query)}')
 
 
 # '''
@@ -147,6 +211,19 @@ if __name__ == "__main__":
   # Run it
 
   # shallow_catalog = get_catalog(55)
+  # print(f'catalog? {shallow_catalog}')
+  catalogs = [shallow_catalog for (shallow_catalog, i, n) in get_catalogs(type="Undergraduate")]
+
+  for catalog in catalogs:
+    for (shallow_course, i, n) in get_courses(catalog["id"]):
+      deep_course = get_course(catalog_id=catalog["id"], course_id=shallow_course["id"])
+      with open(f'./temp/{catalog["id"]}-{shallow_course["id"]}.json', 'w') as f:
+        f.write(json.dumps(deep_course))
+      print(f'{i} / {n}')
+
+  # for (shallow_catalog, i, n) in get_catalogs(type="Undergraduate"):
+  #   print(f'Catalog ID#{shallow_catalog["id"]} \t`{shallow_catalog["catalog-type"]["name"]}`\t -> {shallow_catalog["name"]}')
+  #for (shallow_course, i, n) in get_courses(55, prefix="ENGL", code="1301")
   # if shallow_catalog is None:
   #   print(f'Shallow catalog could not be fetched with catoid={catoid}')
   #   exit(1)
