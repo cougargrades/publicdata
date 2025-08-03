27b101451ddd04c7cc5223875bb5fd118a329b25
Austin Jackson <austinjckson@gmail.com>
2025-08-03 11:45:35 -0500

    mock-database now downloads from the correct bundle instead of always the latest

--- mock-database/src/downloadBundle.ts
+++ mock-database/src/downloadBundle.ts
@@ -2,14 +2,17 @@ import { downloadFile, extractBundle, rimraf } from './_bundleHelper.js';
 
 const DEFAULT_BUNDLE = 'publicdata-bundle.tar.gz';
 export const SELECTED_BUNDLE = process.env.SELECTED_BUNDLE !== undefined ? process.env.SELECTED_BUNDLE : DEFAULT_BUNDLE;
+export const RELEASE_TAG = process.env.RELEASE_TAG !== undefined ? process.env.RELEASE_TAG : 'latest';
 
 // verify that the tmp folder is empty before putting new files there, we don't want old bundles laying around causing problems
 await rimraf('tmp')
 
-await downloadFile(
-  //'https://github.com/cougargrades/publicdata/releases/latest/download/publicdata-testbundle-summer2020.tar.gz',
-  `https://github.com/cougargrades/publicdata/releases/latest/download/${SELECTED_BUNDLE}`,
-  'tmp/bundle.tar.gz'
-)
+const BUNDLE_URL = (
+  RELEASE_TAG === 'latest'
+  ? `https://github.com/cougargrades/publicdata/releases/latest/download/${SELECTED_BUNDLE}`
+  : `https://github.com/cougargrades/publicdata/releases/download/${RELEASE_TAG}/${SELECTED_BUNDLE}`
+);
+
+await downloadFile(BUNDLE_URL, 'tmp/bundle.tar.gz')
 
 await extractBundle('tmp/bundle.tar.gz', 'tmp/test')
