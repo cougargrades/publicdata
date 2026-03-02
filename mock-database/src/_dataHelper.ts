ae5a2fb46ed8d8ef873035ea9198d287d6a45683
Austin Jackson <austinjckson@gmail.com>
2026-03-01 19:48:28 -0600

    maybe this will help?

--- mock-database/src/_dataHelper.ts
+++ mock-database/src/_dataHelper.ts
@@ -1,6 +1,6 @@
 
 //import core_curriculum from '@cougargrades/publicdata/bundle/edu.uh.publications.core/core_curriculum.json' with { type: "json" };
-import core_curriculum from '../tmp/test/edu.uh.publications.core/core_curriculum.json' with { type: "json" };
+//import core_curriculum from '../tmp/test/edu.uh.publications.core/core_curriculum.json' with { type: "json" };
 
 // import { Group } from '@cougargrades/types';
 // import { firebase } from './_firebaseHelper.js';
@@ -10,7 +10,10 @@ import core_curriculum from '../tmp/test/edu.uh.publications.core/core_curriculu
 //   return getCoreCurriculumDocPaths(courseName).map(path => db.doc(path) as FirebaseFirestore.DocumentReference<Group>);
 // }
 
-export function getCoreCurriculumDocPaths(courseName: string): string[] {
+export async function getCoreCurriculumDocPaths(courseName: string): Promise<string[]> {
+
+  const { default: core_curriculum } = await import('../tmp/test/edu.uh.publications.core/core_curriculum.json', { with: { type: 'json' } });
+
   const [department, catalogNumber] = courseName.trim().split(' ')
   return core_curriculum
     .filter((e: any) => e.department === department && e.catalogNumber === catalogNumber) // finds matches
